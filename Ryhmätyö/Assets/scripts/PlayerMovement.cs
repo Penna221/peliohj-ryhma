5bc0e6dcc1d50d415933aef4e918fb25e0847442
Maxim Igolkin <maxim.igolkin@gmail.com>
2022-04-09 03:32:44 +0300

    Juoksun stamina ja taskulampun akku toiminnot
    Lisäsin juoksu ja taskulampun sliderit ja toiminnot.

--- Ryhmätyö/Assets/scripts/PlayerMovement.cs
+++ Ryhmätyö/Assets/scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -18,7 +19,15 @@ public class PlayerMovement : MonoBehaviour
     Light FL; // flushlight
     [SerializeField]
     GameManager gm;
-    
+    [SerializeField]
+    Slider StaminaSlider, FLSlider;
+    [SerializeField]
+    Image StaminaFill;
+    [SerializeField]
+    float staminaValue, StaminaStep, flushlightValue, flushlightStep;
+
+
+
     //ÄÄNI - Lisää vaan playerMovementscriptin kohtaan Map kun siellä on SFXManager.
     [SerializeField]
     SFXManager sm;
@@ -34,7 +43,9 @@ public class PlayerMovement : MonoBehaviour
     private float minPitch = -80f;
     private float maxPitch = 80f;
     Vector3 startPos;
-    
+    private float StaminaMaxValue, FlushliteMaxValue; // voi käyttää jatkossa, jos on jotain itemiä, jotka täyttää staminan/valon
+    private bool staminaDelay = false;
+    private Color staminaFillDefaultColor, flushlightDefaultColor; 
     // Start is called before the first frame update
     void Start()
     {
@@ -44,6 +55,13 @@ public class PlayerMovement : MonoBehaviour
         startPos = transform.position;
         controller = gameObject.AddComponent<CharacterController>();
         FL.enabled = false;
+
+        staminaFillDefaultColor = StaminaFill.color;
+
+        StaminaMaxValue = staminaValue;
+        StaminaSlider.maxValue = StaminaMaxValue;
+        FlushliteMaxValue = flushlightValue;
+        FLSlider.maxValue = FlushliteMaxValue;
     }
 
     // Update is called once per frame
@@ -55,17 +73,20 @@ public class PlayerMovement : MonoBehaviour
         }
         if (Input.GetKeyDown(KeyCode.F))
         {
-            LightSwitch();
+            FlushlighSwitch();
         }
         if(Input.GetKey(KeyCode.LeftShift)){
             running = true;
-            
-        }else{
+        }
+        else{
             running = false;
         }
         Move();
+        Flushlight();
         CameraFollow();   
         CameraRotate();
+        StaminaSlider.value = staminaValue;
+        FLSlider.value = flushlightValue;
     }
 
     void Move()
@@ -79,23 +100,50 @@ public class PlayerMovement : MonoBehaviour
         Vector3 camDir = camera.transform.forward;
         camDir = Vector3.ProjectOnPlane(camDir, Vector3.up);
         transform.forward = camDir;
+
+        if (staminaValue < 0.2)
+        {
+            staminaDelay = true;
+            Color color = new Color(208F / 255f, 48F / 255f, 48F / 255f);
+            StaminaFill.color = color; // väri voi kyl vaihtaa ehkä tummemmaksi
+        }
+        else if (staminaValue > StaminaMaxValue * 0.2) // 20% staminasta  palautua, ennen ku voi juosta
+        {
+            staminaDelay = false;
+            StaminaFill.color = staminaFillDefaultColor;
+        }
+
         if (move != Vector3.zero)
         {
-            if(running){
-                Debug.Log("RUNNING");
-                controller.Move(move * Time.deltaTime * runSpeed);
-                sm.stopWalk();
-                sm.p_run();
-            }else{
+            if (running && staminaValue > 0)
+            {
+                if (!staminaDelay)
+                {
+                    //Debug.Log("RUNNING");
+                    controller.Move(move * Time.deltaTime * runSpeed);
+                    sm.stopWalk();
+                    sm.p_run();
+                    DecreaseStamina();
+                }
+                else {
+                    IncreaseStamina(3);
+                    controller.Move(move * Time.deltaTime * walkSpeed);
+                    sm.p_walk();
+                    sm.stopRun();
+
+                }
+            }
+            else{
                 controller.Move(move * Time.deltaTime * walkSpeed);
                 sm.p_walk();
-
                 sm.stopRun();
+                IncreaseStamina(3);
             }
             gameObject.transform.forward = move;
+        
             //Soitetaan ääni
-            
         }else{
+            IncreaseStamina(1);
             sm.stopWalk();
             sm.stopRun();
         }
@@ -122,9 +170,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 pPos = transform.position;
         camera.transform.position = pPos;
     }
-    void LightSwitch()
+    private void FlushlighSwitch()
     {
-        if (FL.enabled == false)
+        
+        if (FL.enabled == false && flushlightValue > 2)
         {
             FL.enabled = true;
         }
@@ -139,6 +188,43 @@ public class PlayerMovement : MonoBehaviour
         if(collision.collider.tag == "Enemy")
         {
             gm.enemyHit();
+        }
+    }
+
+    private void DecreaseStamina()
+    {
+        if(staminaValue != 0)
+        {
+            staminaValue-= StaminaStep * Time.deltaTime;
+        }
+    }
+    private void IncreaseStamina(float IncreaseSpeed) // IncreaseSpeed - staminan täyttönopeus jakaja
+    {
+        if (staminaValue < StaminaMaxValue)
+        {
+            staminaValue += StaminaStep / IncreaseSpeed * Time.deltaTime;
+        }
+    }
+
+    private void Flushlight()
+    {
+        if (FL.enabled && flushlightValue > 0)
+            DecreaseFlushlight();
+        else if(!FL.enabled)
+            IncreaseFlushlight();
+    }
+    private void DecreaseFlushlight()
+    {
+        if (flushlightValue != 0)
+        {
+            flushlightValue -= flushlightStep * Time.deltaTime;
+        }
+    }
+    private void IncreaseFlushlight()
+    {
+        if (flushlightValue < FlushliteMaxValue)
+        {
+            flushlightValue += flushlightStep / 5 * Time.deltaTime;
         }
     }
 }
---
+++ Ryhmätyö/ReadMe.txt
@@ -0,0 +1,5 @@
+Icon License
+
+Paste this link on the website where your app is available for download or in the description section of the platform or marketplace you’re using.
+
+<a href="https://www.flaticon.com/free-icons/energy" title="energy icons">Energy icons created by Dreamstale - Flaticon</a>
